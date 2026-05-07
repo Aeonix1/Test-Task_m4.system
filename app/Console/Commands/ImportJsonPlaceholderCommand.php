a26018d128a1e8f75eadf4d15ece4b85a86c0354
Aeonix1 <olegg87850@gmail.com>
2026-05-07 12:48:05 +0300

    first commit

---
+++ app/Console/Commands/ImportJsonPlaceholderCommand.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Console\Commands;
+
+use GuzzleHttp\Client;
+use Illuminate\Console\Command;
+
+class ImportJsonPlaceholderCommand extends Command
+{
+    protected $signature = 'import:jsonplaceholder';
+
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $client = new Client(
+            [
+                'base_uri' => 'https://developer-api.m4.systems:4443',
+                'headers' => [
+                    'Authorization' => 'Bearer ' . env('MY_ACCESS_TOKEN'),
+                    'Content-Type' => 'application/json',
+                ]
+            ]
+        );
+
+        // Задание 1 (Авторизация через API)
+        $response = $client->get('/api_auth/login_check', ['auth' => [env("MY_LOGIN"), env("MY_PASSWORD")]]);
+
+        $data = json_decode($response->getBody()->getContents(), true);
+
+        // Задание 2 (Получение URL сервиса SD из ответа авторизации)
+        $services = $data['services'];
+        foreach ($services as $service) {
+            if ($service['code'] === "SD") {
+                $urlSD = $service['apiUrl'];                  // заношу url-адрес SD в отдельную переменную
+                echo($service['apiUrl'] . PHP_EOL);          // вывод url-адреса на экран (выполнение задания 2)
+            }
+        }
+
+
+        // Задание 3 (Получение списка заявок, изменённых за последние 3 дня)
+        $params = [
+            "method" => "M4GetTasks",
+            "params" => ["status" => [0, 1, 4, 5, 6], "lastUpdate" => date('Y-m-d H:i:s', strtotime('-3 day'))],
+            "id" => "1",
+            "jsonrpc" => "2.0",
+        ];
+
+
+        $response = $client->post('/api_web/api.php',
+            [
+                'json' => $params
+            ]);
+        $tasks = json_decode($response->getBody()->getContents(), true);
+
+        // Задание 4
+        if (count($tasks['result']) < 2)
+        {
+            $statusCode = $response->getStatusCode();
+//            return response()->json(['error' => 'error'], 500);
+            echo "Недостаточно заявок для выполнения тестового сценария, $statusCode" . PHP_EOL;
+        }
+
+//        try
+//        {
+//            if (count($tasks['result']) < 2)
+//            {
+//                $task2 = $tasks['result'][1];
+//                echo 'taskId => ' . $task2['taskId'] . ', req => ' . $task2['req'] .', caption => '. $task2['caption']. ', status/statusName => ' . $task2['status']['name'] . PHP_EOL;
+//            }
+//        } catch (\Exception $e)
+//        {
+//            return $e->getCode(0);
+//        }
+
+
+        // Задание 5
+        if (count($tasks['result']) > 2) {
+            $task2 = $tasks['result'][1];
+            echo 'taskId => ' . $task2['taskId'] . ', req => ' . $task2['req'] . ', caption => ' . $task2['caption'] . ', status/statusName => ' . $task2['status']['name'] . PHP_EOL;
+        }
+
+        // Задание 6
+
+        // Добавил изображения по адресу ./public/storage/images/
+
+        // Задание 7
+
+        $image1 = '/public/storage/images/Image1.jpg';
+        $image2 = '/public/storage/images/Image2.jpg';
+        $task2['image1'] = $image1;
+        $task2['image2'] = $image2;
+
+        // Задание 8
+
+        $fio = 'Вержбовский Олег Вячеславович';
+        $datetime = "06.05.2026 17:06";
+        $task2['public_comment'] = "Тестовый комментарий от кандидата: " . $fio . ", " . $datetime;
+
+        // Задание 9 (logout)
+
+        $params2 = [
+            "method" => "logout",
+            "id" => "1",
+            "jsonrpc" => "2.0",
+        ];
+
+        $response = $client->post('/api_auth',
+            [
+                'json' => $params2,
+            ]);
+        json_decode($response->getBody()->getContents());
+    }
+}
